d3859f9650fb94b95af33b2509615b398f1314cd
Ewan Stott <ewan.stott1@gmail.com>
2024-02-01 08:47:04 +0100

    simpsons API setup

---
+++ simpsons-api-react-class-components/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    this.getApiData();
+  }
+
+  getApiData = async () => {
+    const { data } = await axios.get(
+      `https://thesimpsonsquoteapi.glitch.me/quotes?count=50`
+    );
+
+    //put data in state
+    this.setState({ simpsons: data });
+  };
+
+  render() {
+    console.log(this.state);
+    return <></>;
+  }
+}
+
+export default App;
